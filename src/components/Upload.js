67797e3539d6016d3336b28e615612dc7348be50
Berbaas <bdewet867@gmail.com>
2020-11-08 17:36:18 +0200

    added xlsx and started with API

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import S3 from 'react-aws-s3';
+// import S3 from 'react-aws-s3';
 
-/* By directly uploading these files to Amazon S3, I can avoid proxying these requests through my application server. 
-This can significantly reduce network traffic and server CPU usage, and enable my application server to handle other requests during busy periods. 
-S3 also is highly available and durable, making it an ideal persistent store for user uploads. */
-
-const config = {
-  bucketName: process.env.REACT_APP_BUCKET,
-  region: process.env.REACT_APP_S3_REGION,
-  headers: {'Access-Control-Allow-Origin': '*'},
-  accessKeyId: process.env.REACT_APP_AWSACCESSID,
-  secretAccessKey: process.env.REACT_APP_AWSSECRETKEY,
-}
-const ReactS3Client = new S3(config);
+// const config = {
+//   bucketName: process.env.REACT_APP_BUCKET,
+//   region: process.env.REACT_APP_S3_REGION,
+//   headers: {'Access-Control-Allow-Origin': '*'},
+//   accessKeyId: process.env.REACT_APP_AWSACCESSID,
+//   secretAccessKey: process.env.REACT_APP_AWSSECRETKEY,
+// }
+// const ReactS3Client = new S3(config);
 
 class Intro extends Component {
   constructor(props){
     super(props);
     this.state = {
-      fileToUpload: undefined,
-      uploadSuccess: undefined,
-      error: undefined
+      success: false,
+      doneAnalize: false
     };
   }
   
@@ -38,48 +33,84 @@ class Intro extends Component {
   // Perform the upload
   handleUpload = (ev) => {
     let file = this.uploadInput.files[0];
-    console.log("Preparing the upload");
-    
-    if(this.getFileExtension(file.name)==="txt"){
-      axios({
-        method: 'get',
-        url: 'https://0nc3lkfsx0.execute-api.us-east-2.amazonaws.com/default/getPreSignedTXT',
-        responseType: 'stream'
-      })
-        .then(function (response) {
-          console.log(response.data)
-        });
-    }else if(this.getFileExtension(file.name)==="xlsx"){
-      axios({
-        method: 'get',
-        url: 'https://sv5xm4lg4l.execute-api.us-east-2.amazonaws.com/default/getPreSignedXLSX',
-        responseType: 'stream'
-      })
-        .then(function (response) {
-          console.log(response.data)
-        });
-    }
+    let link ="";
+    let headers = "";
 
+    if (this.getFileExtension(file.name) === "txt"){
+      link = "https://0nc3lkfsx0.execute-api.us-east-2.amazonaws.com/default/getPreSignedTXT";
+      headers = "text/plain";
+    }else if(this.getFileExtension(file.name) === "xlsx"){
+      link = "https://sv5xm4lg4l.execute-api.us-east-2.amazonaws.com/default/getPreSignedXLSX";
+      headers = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    }else{
+      throw new Error("Invalid file type");
+    }
+    
+    console.log("Preparing the upload");
+
+    axios.get(link)
+    .then(response => {
+      var url = response.data.uploadURL;
+      console.log("Recieved a signed request " + url);
+
+      var options = {
+        headers: {
+          'Content-Type': headers
+        }
+      };
+      axios.put(url,file,options)
+      .then(result => {
+        console.log("Response from s3")
+        this.setState({success: true});
+      })
+      .catch(error => {
+        alert("ERROR " + JSON.stringify(error));
+      })
+    })
+    .catch(error => {
+      alert(JSON.stringify(error));
+    })
     
   };
+
+  handleAnalyse = (ev) => {
+    let file = this.uploadInput.files[0];
+    let apiUrl = "https://2imdj98e48.execute-api.us-east-2.amazonaws.com/1/extract?extension=" + this.getFileExtension(file.name);
+
+    axios.get(apiUrl)
+    .then(response => {
+      var personalInfo = response.data;
+      //this.setState({url: url})
+      console.log("PI: \n=================\n" + personalInfo);
+    })
+    .catch(error => {
+      alert(JSON.stringify(error));
+    })
+  }
   
   
   render() {
     const Success_message = () => (
       <div style={{padding:50}}>
-        <h3 style={{color: 'green'}}>SUCCESSFUL UPLOAD</h3>
-        <a href={this.state.url}>Access the file here</a>
+        <h2 style={{color: 'green'}}>SUCCESSFUL UPLOAD</h2>
         <br/>
+        <button onClick={this.handleAnalyse}>Analyse my file</button>
       </div>
     )
     return (
       <div className="App">
         <center>
-          <h1>UPLOAD A FILE</h1>
+          <h1>PERSONAL INFORMATION IDENTIFICATION APP</h1>
           {this.state.success ? <Success_message/> : null}
-          <input onChange={this.handleChange} ref={(ref) => { this.uploadInput = ref; }} type="file"/>
-          <br/>
-          <button onClick={this.handleUpload}>UPLOAD</button>
+          {this.state.success ? null :
+            <div>
+              <h2>Upload a file</h2>
+              <input onChange={this.handleChange} ref={(ref) => { this.uploadInput = ref; }} type="file"/>
+              <br/>
+              <button onClick={this.handleUpload}>UPLOAD</button>
+            </div>
+          }
+          
         </center>
       </div>
     );
