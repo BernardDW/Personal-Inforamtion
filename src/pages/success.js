ac2004e19f5a8c5b794ff889913131e522b9bbb9
Berbaas <bdewet867@gmail.com>
2020-11-16 11:27:35 +0200

    Success page updated

--- src/pages/success.js
+++ src/pages/success.js
@@ -9,7 +9,8 @@ export default class Success extends Component {
     render() {
         return (
             <div>
-                <h1>SUCCESS</h1>
+                <h1 style={{marginTop: "2em", marginBottom: "2em"}}>SUCCESS</h1>
+                <img src="https://i.pinimg.com/originals/e8/06/52/e80652af2c77e3a73858e16b2ffe5f9a.gif" alt="Success gif"/>
             </div>
         );
     }
